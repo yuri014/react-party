6ca996e5838dd668371b1ec9b329394c35331fc2
yuri014 <yuriflores2012@gmail.com>
2021-09-11 17:07:23 -0300

    adding app theme

---
+++ src/themes/theme.ts
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    background: '#48466D',
+    secondaryBackground: '#424063',
+    mainPurple: '#9792E3',
+    gray: '#CDCCEA',
+    text: '#fff',
+  },
+  fonts: {
+    primary: '"Poppins", sans-serif',
+  },
+};
+
+export default theme;
